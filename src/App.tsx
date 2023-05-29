a112954aa8412b3e9532b5345a910066ef836129
CoderSleek <dnarula7jan@gmail.com>
2023-05-29 21:29:50 +0530

    docker setup

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+
+
+// Page Components
+// import SignUp from './components/SignUp';
+// import Login from './components/Login';
+// import Home from './components/Home';
+// import UserProfile from './components/UserProfile';
+// import PostDetails from './components/PostDetails';
+// import Chat from './components/Chat';
+// import Topics from './components/Topics';
+// import TopicQuestions from './components/TopicQuestions';
+// import SolveQuestion from './components/SolveQuestion';
+// import Leaderboard from './components/Leaderboard';
+// import AdminPanel from './components/AdminPanel';
+// import NewQuestion from './components/NewQuestion';
+
+
+const App = () => {
+  return (
+    // <BrowserRouter>
+    //  <Routes>
+    //     <Route  path="/signup" element={SignUp} />
+    //     <Route  path="/login" element={Login} />
+    //     <Route  path="/home" element={Home} />
+    //     <Route  path="/user/:id" element={UserProfile} />
+    //     <Route  path="/post/:id" element={PostDetails} />
+    //     <Route  path="/chat" element={Chat} />
+    //     <Route  path="/topics" element={Topics} />
+    //     <Route  path="/topics/:topic/questions" element={TopicQuestions} />
+    //     <Route  path="/topics/:topic/questions/:id/solve" element={SolveQuestion} />
+    //     <Route  path="/leaderboard" element={Leaderboard} />
+    //     <Route  path="/admin" element={AdminPanel} />
+    //     <Route  path="/newquestion" element={NewQuestion} />
+    //  </Routes>
+    // </BrowserRouter>
+    <>What is love? Baby don't hurt me, don't hurt me. No more</>
+  )
+}
+
+
+export default App;
