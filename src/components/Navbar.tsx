74cbf3a0f08491f806879a986ff2e345e41258fa
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-06-06 20:11:50 +0530

    Update code strucutre Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,36 +12,63 @@ const Navbar = () => {
 
   return (
     <div>
-      <ul id="nav" className='flex flex-col bg-black w-20 h-screen text-white'>
+      <ul 
+        id="nav" 
+        className='flex flex-col bg-black w-20 h-screen text-white'
+      >
 
         {/* Problem description */}
 
-        <li className={`flex items-center justify-center ${activePage === 'Description' ? "bg-orange-500 text-black" : ""}`}>
-          <a href="#" className={`h-20 flex items-center justify-center w-full ${activePage === 'Description' ? "bg-orange-500 text-black" : ""}`} onClick={() => setActivePage("Description")}>
+        <li 
+          className={`flex items-center justify-center ${activePage === 'Description' ? "bg-orange-500 text-black" : ""}`}
+        >
+          <a 
+            href="#" 
+            className={`h-20 flex items-center justify-center w-full ${activePage === 'Description' ? "bg-orange-500 text-black" : ""}`} 
+            onClick={() => setActivePage("Description")}
+          >
             Desc
           </a>
         </li>
 
         {/* Solutions. All the other users solution + official solution by us will be here. */}
 
-        <li className={`flex items-center justify-center ${activePage === 'Solution' ? "bg-orange-500 text-black" : ""}`}>
-          <a href="#" className={`h-20 flex items-center justify-center w-full ${activePage === 'Solution' ? "bg-orange-500 text-black" : ""}`} onClick={() => setActivePage("Solution")}>
+        <li
+          className={`flex items-center justify-center ${activePage === 'Solution' ? "bg-orange-500 text-black" : ""}`}
+        >
+          <a 
+            href="#" 
+            className={`h-20 flex items-center justify-center w-full ${activePage === 'Solution' ? "bg-orange-500 text-black" : ""}`} 
+            onClick={() => setActivePage("Solution")}
+          >
             Solutions
           </a>
         </li>
 
         {/* Here, we'll just have simple discussions. No answers. Small hints are allowed. */}
 
-        <li className={`flex items-center justify-center ${activePage === 'Discussion' ? "bg-orange-500 text-black" : ""}`}>
-          <a href="#" className={`h-20 flex items-center justify-center w-full ${activePage === 'Discussion' ? "bg-orange-500 text-black" : ""}`} onClick={() => setActivePage("Discussion")}>
+        <li 
+          className={`flex items-center justify-center ${activePage === 'Discussion' ? "bg-orange-500 text-black" : ""}`}
+        >
+          <a 
+            href="#" 
+            className={`h-20 flex items-center justify-center w-full ${activePage === 'Discussion' ? "bg-orange-500 text-black" : ""}`} 
+            onClick={() => setActivePage("Discussion")}
+          >
             Discussion
           </a>
         </li>
 
         {/* All of the users submissions will be here. */}
 
-        <li className={`flex items-center justify-center ${activePage === 'Submission' ? "bg-orange-500 text-black" : ""}`}>
-          <a href="#" className={`h-20 flex items-center justify-center w-full ${activePage === 'Submission' ? "bg-orange-500 text-black" : ""}`} onClick={() => setActivePage("Submission")}>
+        <li 
+          className={`flex items-center justify-center ${activePage === 'Submission' ? "bg-orange-500 text-black" : ""}`}
+        >
+          <a 
+            href="#" 
+            className={`h-20 flex items-center justify-center w-full ${activePage === 'Submission' ? "bg-orange-500 text-black" : ""}`} 
+            onClick={() => setActivePage("Submission")}
+          >
             Submission
           </a>
         </li>
